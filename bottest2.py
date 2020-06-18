085401f555eadf34ae45bfcbd0414fc0cfc284d5
Kian Murdock <kianmurdock07@gmail.com>
2020-06-18 17:51:53 -0400

    update bot to work with discord api changes

--- bottest2.py
+++ bottest2.py
@@ -1,9 +1,12 @@
 import random, asyncio
 from discord.ext.commands import Bot
 from discord import Game
+import os
+print (os.listdir('.'))
 
+token = open("tokenFile.txt", 'r')
 BOT_PREFIX = ("?", "!")
-TOKEN = ""
+TOKEN = token.read()
 client = Bot(command_prefix=BOT_PREFIX)
 
 #########################################################################
@@ -26,33 +29,34 @@ async def eight_ball(context):
         'Ask Cow'
     ]
     choice = random.randint(0,len(possible_responses)-1)
-    await client.say(possible_responses[choice] + ". " + context.message.author.mention)
+    await context.send(possible_responses[choice] + ". " + context.message.author.mention)
 
 # Square number command
 @client.command(name='sq',
                 description='Responds with the number squared',
-                brief='squares a number',
-                aliases='square')
-async def square(number):
+                brief='squares a number')
+async def square(context, number):
     num_squared = (int(number) * int(number))
-    await client.say(str(number) + " squared is " + str(num_squared))
+    await context.send(str(number) + " squared is " + str(num_squared))
 
 # Coin flip command
 @client.command(name='coinflip')
-async def coin_flip(h = "Heads", t = "Tails"):
+async def coin_flip(context):
+    h = "Heads"
+    t = "Tails"
     flip = int(random.random()*10)
     if flip<5:
         result = h
     elif flip>=5:
         result = t
 
-    await client.say("Magic bot-coin says: " + str(result))
+    await context.send("Magic bot-coin says: " + str(result))
 
 # Picker command
 @client.command(aliases=['pick', 'choose', 'pickforme', 'decide'])
-async def picker(*args):
+async def picker(context, *args):
     r = random.randint(0, len(args)-1)
-    await client.say("I choose... " + args[r])
+    await context.send("I choose... " + args[r])
 
 """ # rps command
 @client.command()
@@ -69,13 +73,13 @@ async def rps(play):
 @client.command()
 async def deaths(game):
     if game == 'ds3':   #DARK SOULS 3
-        await client.say("123 deaths")
+        await game.send("123 deaths")
     elif game == 'ds2': #DARK SOULS 2
-        await client.say("309 deaths. But like half of those don't count.")
+        await game.send("309 deaths. But like half of those don't count.")
     elif game == 'ds1': #DARK SOULS 1
-        await client.say("33 so far.")
+        await game.send("33 so far.")
     elif game == 'ds3sl1': #DARK SOULS 3
-        await client.say("29 deaths")
+        await game.send("29 deaths")
 
 #########################################################################
 #                               EVENTS                                  #
@@ -84,14 +88,14 @@ async def deaths(game):
 # confirms login in terminal
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name="with meatbags"))    
+    #await client.change_presence(game=Game(name="with meatbags"))    
     print("Logged in as " + client.user.name)
 
 async def list_servers():
     await client.wait_until_ready()
     while not client.is_closed:
         print("Current servers: ")
-        for server in client.servers:
+        for server in client.guilds:
             print(server.name)
         await asyncio.sleep(600)
 
